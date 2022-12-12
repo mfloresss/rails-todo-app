f4f88ccb8462cf6abe47c38c4509b30b90ba4775
Micha <mich4el.flores@gmail.com>
2022-12-12 15:36:14 -0300

    Format code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,66 +1,60 @@
 class TasksController < ApplicationController
-    before_action :set_task
-    skip_before_action :set_task, only: [:index, :new, :create, :destroy_all]
+  before_action :set_task
+  skip_before_action :set_task, only: %i[index new create destroy_all]
 
-    def index
-        @tasks = Task.all
-        respond_to do |format|
-            format.html
-            format.json { render json: @tasks, status: :ok }
-        end
+  def index
+    @tasks = Task.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @tasks, status: :ok }
     end
+  end
 
-    def show
-        @comment = @task.comments.build
-        # @task = @task.attributes.merge( { comments: @task.comments } )
-        # respond_to do |format|
-        #     format.html
-        #     format.json { render json: @task, include: [:comments], status: :ok } 
-        # end
+  def show
+    @comment = @task.comments.build
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @task = Task.new
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @task = Task.new(task_params)
-        if @task.save
-            redirect_to @task
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
+  def destroy
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
 
-    def edit 
-    end
+  def destroy_all
+    @tasks = Task.all
+    @tasks.destroy_all
+    redirect_to root_path, status: :see_other
+  end
 
-    def update
-        if @task.update(task_params)
-            redirect_to @task
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def destroy
-        @task.destroy
-        redirect_to root_path, status: :see_other
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    def destroy_all
-        @tasks = Task.all
-        @tasks.destroy_all
-        redirect_to root_path, status: :see_other
-    end
-
-    private
-
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
-    def task_params
-        params.require(:task).permit(:title, :body, :commenter, :comment)
-    end
+  def task_params
+    params.require(:task).permit(:title, :body, :commenter, :comment)
+  end
 end
