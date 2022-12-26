ff77f3da1193af99e7ca91ca3169e9aa40b63fef
Micha <mich4el.flores@gmail.com>
2022-12-26 10:25:32 -0300

    User settings changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :tasks
+  has_many :invitation_teams
   has_and_belongs_to_many :teams
 
   validates :name, :last_name, :email, presence: { message: "Can't be empty" }
+
+  def full_name
+    name + ' ' + last_name
+  end
 end
