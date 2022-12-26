fee9845e0aefcd144b5e8e9c097940e9ca59fb6f
Micha <mich4el.flores@gmail.com>
2022-12-26 13:14:04 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  { name: 'Michael', last_name: 'Flores', email: 'micha@gmail.com', password: 'micha1?' },
+  { name: 'Pedro', last_name: 'Perez', email: 'pedro@gmail.com', password: 'pedro1?' },
+  { name: 'Gonzalo', last_name: 'Rodriguez', email: 'gonza@gmail.com', password: 'gonza1?' }
+]
+
+tasks = [
+  { title: 'Go to the toilet' },
+  { title: 'Take wather' }
+]
+
+counts = {
+  users_created: 0,
+  teams_created: 0,
+  tasks_created: 0
+}
+
+users.each do |user|
+  new_user = User.new(user)
+
+  new_user.save!
+
+  new_team = Team.new(user_id: new_user[:id], name: "#{new_user[:name]}'s Team")
+
+  new_user.teams << new_team
+
+  tasks.each do |task|
+    new_team.tasks.new(user: new_user, **task).save!
+    counts[:tasks_created] += 1
+  end
+
+  counts[:users_created] += 1
+  counts[:teams_created] += 1
+end
+
+puts 'Counts'
+25.times { print '-' }
+puts "\n> Users created: #{counts[:users_created]}"
+puts "> Teams created: #{counts[:teams_created]}"
+puts "> Tasks created: #{counts[:tasks_created]}"
